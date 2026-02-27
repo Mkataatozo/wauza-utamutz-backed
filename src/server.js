2212286272bf1246f773c6061d3428bdc9933b0c
Yohana Michael <yohanamichael92@gmail.com>
2026-02-27 17:56:34 +0300

    added api route

--- src/server.js
+++ src/server.js
@@ -6,11 +6,17 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// Main test route
 app.get("/", (req, res) => {
   res.send("Wauza Utamutz Backend Running 🚀");
 });
 
-const PORT = 5000;
+// API test route
+app.get("/api", (req, res) => {
+  res.send("API working ✅");
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
